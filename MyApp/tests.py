c99369bc5c7266c806318e87359cbdace43a7fd1
Patrick Clary <patrickm@theclarys.org>
2023-02-15 22:59:44 -0600

    Add index view unit test

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.http import HttpRequest
+from .views import index
 
 # Create your tests here.
+class IndexViewTestCase(TestCase):
+    def test_index_view_loads(self):
+        pass
+
+class IndexViewTestCase(TestCase):
+    def test_index_view_loads(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
