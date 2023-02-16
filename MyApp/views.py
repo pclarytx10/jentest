b97b101e0a5684bdaa233e704375c8d603d47cb3
Patrick Clary <patrickm@theclarys.org>
2023-02-16 13:18:34 -0600

    Add home and about views. Base template.

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Coin
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello World")
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
+
+
+# Define the about view
+def about(request):
+    return render(request, 'about.html')
