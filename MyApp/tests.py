8ea6c9f844c5643cbb413b577697343a9ccf1969
Patrick Clary <patrickm@theclarys.org>
2023-02-16 08:58:30 -0600

    pep8 testing

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -5,10 +5,12 @@ from .views import index
 
 # Create your tests here.
 
+
 class IndexViewTestCase(TestCase):
 
 
     def test_index_view_loads(self):
+
         pass
 
 
@@ -16,6 +18,7 @@ class IndexViewTestCase(TestCase):
 
 
     def test_index_view_loads(self):
+
         request = HttpRequest()
         response = index(request)
         self.assertEqual(response.status_code, 200)
