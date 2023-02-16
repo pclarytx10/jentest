82627c9ec708e87a39429c8e66dfb733af15ce10
Patrick Clary <patrickm@theclarys.org>
2023-02-16 08:48:04 -0600

    Pep8 testing

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -4,14 +4,15 @@ from django.http import HttpRequest
 from .views import index
 
 # Create your tests here.
-class IndexViewTestCase(TestCase):
-    
+
+
+class IndexViewTestCase(TestCase):   
     
     def test_index_view_loads(self):
         pass
 
-class IndexViewTestCase(TestCase):
-    
+
+class IndexViewTestCase(TestCase):    
     
     def test_index_view_loads(self):
         request = HttpRequest()
