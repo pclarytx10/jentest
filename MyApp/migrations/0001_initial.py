b960f4cca71ce45bafe790c9b53c231dd553a811
Patrick Clary <patrickm@theclarys.org>
2023-02-16 18:46:02 -0600

    Pep8 migrations file fix

--- MyApp/migrations/0001_initial.py
+++ MyApp/migrations/0001_initial.py
@@ -14,7 +14,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Coin',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, 
+                        primary_key=True, serialize=False, 
+                        verbose_name='ID')),
                 ('coin_name', models.CharField(max_length=50)),
                 ('api_id', models.CharField(blank=True, max_length=250)),
                 ('coin_symbol', models.CharField(blank=True, max_length=10)),
